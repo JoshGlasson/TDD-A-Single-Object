2c59ff5fe725fa1fb97c936998826604ee01a1c6
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-21 13:21:51 +0000

    refactored sum code and a couple of tests

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,6 +1,6 @@
 class Dice
 attr_reader :number_of_dice
-attr_reader :rolls
+attr_reader :dice_rolls
 attr_reader :dice_faces
 ONE_DICE = 1
 SIX_SIDED = 6
@@ -8,20 +8,27 @@ SIX_SIDED = 6
   def initialize(number_of_dice = ONE_DICE, dice_faces = SIX_SIDED)
     @number_of_dice = number_of_dice
     @dice_faces = dice_faces
-    @rolls = []
+    @dice_rolls = []
   end
 
-  def roll
+  def roll_dice
     number_of_dice.times do
-      @rolls << rand(1..dice_faces)
+      @dice_rolls << rand(1..dice_faces)
     end
-    return rolls
+    return dice_rolls
   end
 
   def score
-    sum_of_rolls = 0
-    rolls.each { |dice| sum_of_rolls += dice }
-    return "#{rolls.join(", ")} - Total Score: #{sum_of_rolls}"
+    sum_of_rolls = dice_rolls.inject(0) { |sum, dice| sum + dice }
+    return "#{dice_rolls.join(", ")} - Total Score: #{sum_of_rolls}"
   end
 
 end
+
+def new_roll(dice, dice_size)
+  Dice.new(dice, dice_size)
+end
+
+#two_d20 = new_roll(2, 20)
+#two_d20.roll_dice
+#puts two_d20.score
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,36 +3,34 @@ require 'dice'
 describe Dice do
 
   it 'rolls a dice' do
-    expect(subject).to respond_to :roll
+    expect(subject).to respond_to :roll_dice
   end
 
   it 'rolls dice and returns number' do
-    expect(subject.roll).to eq subject.rolls
+    expect(subject.roll_dice).to eq subject.dice_rolls
   end
 
   it 'rolls multiple dice' do
     dice = Dice.new(2)
-    dice.roll
-    expect(dice.rolls.count).to eq 2
+    dice.roll_dice
+    expect(dice.dice_rolls.count).to eq 2
   end
 
   it 'defaults to one dice if no number entered' do
-    subject.roll
-    expect(subject.rolls.count).to eq Dice::ONE_DICE
+    subject.roll_dice
+    expect(subject.dice_rolls.count).to eq Dice::ONE_DICE
   end
 
   it 'shows result of each dice roll' do
-    dice = Dice.new(2)
-    dice.roll
-    sum_of_rolls = 0
-    dice.rolls.each { |dice| sum_of_rolls += dice }
-    expect(dice.score).to eq "#{dice.rolls.join(", ")} - Total Score: #{sum_of_rolls}"
+    dice = Dice.new(2, 1)
+    dice.roll_dice
+    expect(dice.score).to eq "1, 1 - Total Score: 2"
   end
 
   it 'can roll different sized dice' do
     dice = Dice.new(Dice::ONE_DICE, 1)
-    dice.roll
-    expect(dice.rolls).to eq [1]
+    dice.roll_dice
+    expect(dice.dice_rolls).to eq [1]
   end
 
   it 'defaults to 6 sided die if left blank' do
@@ -42,7 +40,7 @@ describe Dice do
 
   it 'can roll different sized dice multiple times' do
     dice = Dice.new(2, 1)
-    dice.roll
-    expect(dice.rolls).to eq [1,1]
+    dice.roll_dice
+    expect(dice.dice_rolls).to eq [1,1]
   end
 end
